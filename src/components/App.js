d7df8e4fa3bdad5ad9c239cbb9c0c038db1f644f
plam <plam@localhost.dev>
2021-02-05 11:06:20 +0000

    05_06 Create Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
+
+import Header from "./common/Header";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 
 function App() {
   return (
     <div className="container-fluid">
+      <Header />
       <Route path="/" exact component={HomePage} />
       <Route path="/about" component={AboutPage} />
     </div>
