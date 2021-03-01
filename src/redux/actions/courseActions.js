0a0818c510ac358fcb6f54a92968380761c9c910
plam <plam@localhost.dev>
2021-03-01 13:26:32 +0000

    11_10 Add Save Course Thunk and Action Creators

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,11 +1,15 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
 }
 
-function loadCoursesSuccess(courses) {
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+export function loadCoursesSuccess(courses) {
   return {
     type: types.LOAD_COURSES_SUCCESS,
     courses,
@@ -18,6 +22,21 @@ export function loadCourses() {
       .getCourses()
       .then((courses) => {
         dispatch(loadCoursesSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch) {
+    return courseApi
+      .saveCourse(course)
+      .then((savedCourse) => {
+        savedCourse.id
+          ? dispatch(createCourseSuccess(savedCourse))
+          : dispatch(updateCourseSuccess(savedCourse));
       })
       .catch((error) => {
         throw error;
