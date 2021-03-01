ab10b77291a25f1c240ab4999dda9d4f2eb0deb8
plam <plam@localhost.dev>
2021-03-01 11:42:14 +0000

    11_08 Call CourseForm on ManageCoursePage

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,10 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { loadCourses } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
+import { newCourse } from "../../../tools/mockData";
+import CourseForm from "./CourseForm";
 
-const ManageCoursePage = ({ courses, authors, loadCourses, loadAuthors }) => {
+const ManageCoursePage = ({
+  courses,
+  authors,
+  loadCourses,
+  loadAuthors,
+  ...props
+}) => {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
+
   useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch((error) =>
@@ -19,16 +30,13 @@ const ManageCoursePage = ({ courses, authors, loadCourses, loadAuthors }) => {
     }
   }, []);
 
-  return (
-    <>
-      <h2>Manage Course</h2>
-    </>
-  );
+  return <CourseForm course={course} authors={authors} errors={errors} />;
 };
 
 ManageCoursePage.propTypes = {
   loadCourses: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired,
+  course: PropTypes.object.isRequired,
   courses: PropTypes.array.isRequired,
   authors: PropTypes.array.isRequired,
 };
@@ -37,6 +45,7 @@ function mapStateToProps(state) {
   return {
     courses: state.courses,
     authors: state.authors,
+    course: newCourse,
   };
 }
 
