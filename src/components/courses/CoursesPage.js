d891f33d53299bf8a7db7b744c5f6bd0f6407fe0
plam <plam@localhost.dev>
2021-03-02 17:48:36 +0000

    12_02 Create Spinner Component

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -6,6 +6,7 @@ import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
 import { Redirect } from "react-router-dom";
+import Spinner from "../common/Spinner";
 
 class CoursesPage extends React.Component {
   state = {
@@ -39,6 +40,7 @@ class CoursesPage extends React.Component {
         >
           Add Course
         </button>
+        <Spinner />
         <CourseList courses={this.props.courses} />
       </>
     );
